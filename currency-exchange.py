6e3a3f2a9ad4a22983d7b41de236073a6d286fef
pakagrong-lebel <92211586+pakagrong-lebel@users.noreply.github.com>
2023-10-29 15:58:24 +1100

    Add files via upload

---
+++ currency-exchange.py
@@ -0,0 +1,3 @@
+# Python script to fetch currency exchange rates
+# Your code here to connect to a currency exchange API (e.g., Fixer.io, Open Exchange Rates)
+# Your code here to perform currency conversions and display exchange rates
